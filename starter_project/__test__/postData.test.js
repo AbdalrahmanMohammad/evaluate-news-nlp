44a650a2634af742a3a1b6deddb1a36ebc700342
Abdalrahman-22 <abdalrahman.s.mohammad@gmail.com>
2024-08-19 19:54:13 +0300

    change the name

--- starter_project/__test__/postData.test.js
+++
@@ -1,7 +0,0 @@
-import { postData } from "../src/client/js/formHandler"
-
-describe("Testing the posting Data functionality", () => {
-    test("Testing the postData() function", () => {
-        expect(postData).toBeDefined();
-    })
-});
